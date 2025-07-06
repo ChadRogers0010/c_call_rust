bb7f4d527edcc119ead07474551319f8974cb982
chad <chadrogers0010@gmail.com>
2025-07-06 00:36:18 -0700

    Change derived FIB_LEN to literal

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,18 +1,9 @@
-const fn get_fib_len() -> usize {
-    let mut a = 0;
-    let mut b = 1;
-    let mut len = 1;
-    while let Some(c) = u32::checked_add(a, b) {
-        a = b;
-        b = c;
-        len += 1;
-    }
-    len
-}
-pub const FIB_LEN: usize = get_fib_len();
+/// Number of fibonacci values
+/// that fit in uint32_t
+pub const FIB_LEN: usize = 47;
 
-const fn make_fib_table() -> [u32; FIB_LEN] {
-    let mut arr = [0; FIB_LEN];
+const fn make_fib_table() -> [u32; 47] {
+    let mut arr = [0; 47];
     arr[1] = 1;
     let mut idx = 2;
     while idx < arr.len() {
@@ -21,7 +12,10 @@ const fn make_fib_table() -> [u32; FIB_LEN] {
     }
     arr
 }
-pub const FIB_TABLE: [u32; FIB_LEN] = make_fib_table();
+/// An array of uint32_t fibonacci values
+/// going from f(0) to f(46)
+#[unsafe(no_mangle)]
+pub static FIB_TABLE: [u32; 47] = make_fib_table();
 
 /// Reads a lookup table for the values between 0 and 46.
 /// Going over 46 returns zero.
@@ -34,18 +28,29 @@ pub const extern "C" fn fib_get(n: u8) -> u32 {
     FIB_TABLE[n]
 }
 
-#[unsafe(no_mangle)]
-pub const extern "C" fn double_num(n: u32) -> u32 {
-    n * 2
-}
-
 #[cfg(test)]
 mod tests {
-    use crate::fib_get;
+    use crate::{FIB_LEN, fib_get};
 
     #[test]
     fn fib_get_test() {
         let fib_num = fib_get(7);
         assert!(fib_num == 13);
+
+        let fib_num = fib_get(47);
+        assert!(fib_num == 0);
+    }
+
+    #[test]
+    fn fib_max_len() {
+        let mut a = 0;
+        let mut b = 1;
+        let mut len = 1;
+        while let Some(c) = u32::checked_add(a, b) {
+            a = b;
+            b = c;
+            len += 1;
+        }
+        let _: () = assert!(len == FIB_LEN);
     }
 }
--- src/main.c
+++ src/main.c
@@ -4,6 +4,9 @@ int main(int argc, char *argv[]) {
   printf("In main\n");
   int idx = 7;
   int fib_num = fib_get(7);
-  printf("fib_num(%d) -> %d", idx, fib_num);
+  printf("fib_num(%d) -> %d\n", idx, fib_num);
+  for (int i = 0; i < 10; i++) {
+    printf("fib %d: %d\n", i, FIB_TABLE[i]);
+  }
   return 0;
 }
